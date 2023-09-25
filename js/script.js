016724f38f3842610ab2ab9b123d15c8b51757cc
budionoaryawisanto <budionoarya.w@gmail.com>
2023-09-25 17:30:19 +0700

    finishing

--- js/script.js
+++ js/script.js
@@ -9,6 +9,18 @@ $(".page-scroll").on("click", function (e) {
 
   e.preventDefault();
 });
-const form = document.querySelectorAll('form');
+const fname = document.getElementById('fname');
 
-
+  function changeName(e) {
+    document.getElementById('name').innerHTML = fname.value;
+    e.preventDefault()
+}
+  
+function show() {
+  document.getElementById('form').classList.remove('d-none')
+  document.getElementById('edit').classList.add('d-none')
+}
+function hide() {
+  document.getElementById('form').classList.add('d-none')
+  document.getElementById('edit').classList.remove('d-none')
+}
